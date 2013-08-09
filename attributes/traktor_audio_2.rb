3899a0de1e7e965597c3231cf8dbdb9f0bd1890d
James Cuzella <james.cuzella@lyraphase.com>
2013-08-09 17:19:49 -0600

    Adding Traktor Audio 2 driver recipe

---
+++ attributes/traktor_audio_2.rb
@@ -0,0 +1,6 @@
+default['lyraphase_workstation']['traktor_audio_2']['dmg']['source']      = 'http://protected-cdn-ec.native-instruments.com/um/traktorpro2/Traktor_Audio_2_253_Mac_p.dmg?dc979dd33ddbd9aba0c89ece06469e3280344c4dcb673c67415e41480fd87a51b299420f97ae0920015500ef6eab190f742e7bfa9694dec41b92a5203c69a7af8b221766f5a9f504ea133a75f283926797be2b5fa1e609e3322ab801e2'
+default['lyraphase_workstation']['traktor_audio_2']['dmg']['checksum']    = "39d4f45c4e9aaa32b882a39b175997173d07040094f5888c2e3ef72667753083"
+default['lyraphase_workstation']['traktor_audio_2']['dmg']['volumes_dir'] = "Traktor Audio 2"
+default['lyraphase_workstation']['traktor_audio_2']['dmg']['dmg_name']    = 'TraktorAudio2_Mac.dmg'
+default['lyraphase_workstation']['traktor_audio_2']['dmg']['app']         = 'Traktor Audio 2 2.5.3 Installer Mac'
+default['lyraphase_workstation']['traktor_audio_2']['dmg']['type']        = 'pkg'
---
+++ recipes/traktor_audio_2.rb
@@ -0,0 +1,13 @@
+dmg_properties = node['lyraphase_workstation']['traktor_audio_2']['dmg']
+
+dmg_package "Traktor Audio 2" do
+  source      dmg_properties['source']
+  checksum    dmg_properties['checksum']
+  volumes_dir dmg_properties['volumes_dir']
+  dmg_name    dmg_properties['dmg_name']
+  app         dmg_properties['app']
+  type        dmg_properties['type']
+  owner       node['current_user']
+  # package_id  'com.native-instruments.Traktor2.*'
+  action :install
+end
