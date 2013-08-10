b900fa993fdb974c23e4712dff2f7030375c8da1
James Cuzella <james.cuzella@lyraphase.com>
2013-08-09 21:03:37 -0600

    WIP on OmniFocus license auto-configuration

--- recipes/omnifocus.rb
+++ recipes/omnifocus.rb
@@ -8,3 +8,17 @@ dmg_package "OmniFocus" do
   accept_eula true
   #package_id  'com.native-instruments.Traktor2.*'
 end
+
+# TODO: Install license, remove sprout attribute dependency
+# app_supportdir = "#{node['sprout']['home']}/Library/Application Support"
+
+# recursive_directories([app_supportdir, "Omni Group", "Software Licenses/"]) do
+#   owner node['current_user']
+# end
+
+# template File.join(app_supportdir, 'DaisyDisk', 'License.DaisyDisk') do
+#   source "License.DaisyDisk.erb"
+#   owner node['current_user']
+#   variables :license => node['lyraphase_workstation']['omnifocus']['license']
+#   not_if { node['lyraphase_workstation']['omnifocus']['license']['owner'].nil? || node['lyraphase_workstation']['omnifocus']['license']['key'].nil? }
+# end
