bd44dd2f065031325f1c1d97f3e039e79e9cbf89
James Cuzella <james.cuzella@lyraphase.com>
2013-09-06 14:50:57 -0600

    Remove .dmg from Traktor Audio 2 dmg_name

--- attributes/traktor_audio_2.rb
+++ attributes/traktor_audio_2.rb
@@ -1,6 +1,6 @@
 default['lyraphase_workstation']['traktor_audio_2']['dmg']['source']      = 'http://lyraphase.com/doc/installers/mac/Traktor_Audio_2_253_Mac_p.dmg'
 default['lyraphase_workstation']['traktor_audio_2']['dmg']['checksum']    = "39d4f45c4e9aaa32b882a39b175997173d07040094f5888c2e3ef72667753083"
 default['lyraphase_workstation']['traktor_audio_2']['dmg']['volumes_dir'] = "Traktor Audio 2"
-default['lyraphase_workstation']['traktor_audio_2']['dmg']['dmg_name']    = 'TraktorAudio2_Mac.dmg'
+default['lyraphase_workstation']['traktor_audio_2']['dmg']['dmg_name']    = 'TraktorAudio2_Mac'
 default['lyraphase_workstation']['traktor_audio_2']['dmg']['app']         = 'Traktor Audio 2 2.5.3 Installer Mac'
 default['lyraphase_workstation']['traktor_audio_2']['dmg']['type']        = 'pkg'
