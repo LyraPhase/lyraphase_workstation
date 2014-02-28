1c9331a847027bee4dd990bf4ff61b89906c9793
James Cuzella <james.cuzella@lyraphase.com>
2014-02-27 22:33:26 -0700

    Adding multibit app

---
+++ attributes/multibit.rb
@@ -0,0 +1,4 @@
+default['sprout']['multibit']['dmg']['source']      = 'https://multibit.org/releases/multibit-0.5.16/multibit-0.5.16.dmg'
+default['sprout']['multibit']['dmg']['checksum']    = '94f673b54b90f7aa35cac2fab08a85d26bdab8eebcb751e41256c853b045c25a'
+default['sprout']['multibit']['dmg']['volumes_dir'] = 'MultiBit-mac'
+default['sprout']['multibit']['dmg']['type']        = 'app'
--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,8 @@ maintainer_email "james.cuzella@lyraphase.com"
 license          "Apache 2.0"
 description      "Recipes to Install & Configure my workstation"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
+supports         'mac_os_x'
+depends          'homebrew'
+depends          'dmg'
