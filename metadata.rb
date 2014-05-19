3898fcc636f649d9d8bd596e704adc8abfdc2242
James Cuzella <james.cuzella@lyraphase.com>
2014-05-19 12:30:57 -0600

    Bump cookbook version to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "james.cuzella@lyraphase.com"
 license          "Apache 2.0"
 description      "Recipes to Install & Configure my workstation"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.2.0"
 
 supports         'mac_os_x'
 depends          'homebrew'
