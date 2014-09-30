6939f39cd9697f8da06ed9aded418a7ed88cddd1
James Cuzella <james.cuzella@lyraphase.com>
2014-09-30 01:09:49 -0600

    Swapping out Apache2 for GPLv3 License; adding license to all recipes

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             "lyraphase_workstation"
 maintainer       "James Cuzella"
 maintainer_email "james.cuzella@lyraphase.com"
-license          "Apache 2.0"
+license          "GNU Public License 3.0"
 description      "Recipes to Install & Configure my workstation"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 
 supports         'mac_os_x'
 depends          'homebrew'
