e3da67a6fc5311e9f0dd910a7d7d77130302ba1a
James Cuzella <james.cuzella@lyraphase.com>
2015-01-26 09:57:32 -0700

    Adding daisydisk app

---
+++ attributes/daisydisk.rb
@@ -0,0 +1,5 @@
+default['lyraphase_workstation']['daisydisk']['dmg']['source']      = 'http://www.daisydiskapp.com/downloads/DaisyDisk.dmg'
+default['lyraphase_workstation']['daisydisk']['dmg']['checksum']    = 'd0a606dee19e524d6fa7b79fd48b3b9865123ca4126fb8805f8e96c317b57b31'
+default['lyraphase_workstation']['daisydisk']['dmg']['volumes_dir'] = 'DaisyDisk 2'
+default['lyraphase_workstation']['daisydisk']['license']['customer_name']    = nil
+default['lyraphase_workstation']['daisydisk']['license']['registration_key'] = nil
