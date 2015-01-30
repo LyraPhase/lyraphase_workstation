5c35eafe6d027df5d127f5659cf24a6e9384ba4d
James Cuzella <james.cuzella@lyraphase.com>
2015-01-29 18:52:26 -0700

    lyraphase_workstation::daisydisk: Add license via encryped data_bag

--- recipes/daisydisk.rb
+++ recipes/daisydisk.rb
@@ -13,9 +13,15 @@ recursive_directories([app_supportdir, "DaisyDisk"]) do
   owner node['current_user']
 end
 
+license_data = Chef::EncryptedDataBagItem.load('lyraphase_workstation', 'daisydisk_license') rescue nil
+
+unless license_data.nil? && ! node['lyraphase_workstation']['daisydisk']['license'].nil? && ! node['lyraphase_workstation']['daisydisk']['license']['customer_name'].nil? && ! node['lyraphase_workstation']['daisydisk']['license']['registration_key'].nil?
+  license_data = node['lyraphase_workstation']['daisydisk']['license']
+end
+
 template File.join(app_supportdir, 'DaisyDisk', 'License.DaisyDisk') do
   source "License.DaisyDisk.erb"
   owner node['current_user']
-  variables :license => node['lyraphase_workstation']['daisydisk']['license']
-  not_if { node['lyraphase_workstation']['daisydisk']['license']['customer_name'].nil? || node['sprout']['daisydisk']['license']['registration_key'].nil? }
+  variables :license => license_data
+  not_if { license_data.nil? }
 end
