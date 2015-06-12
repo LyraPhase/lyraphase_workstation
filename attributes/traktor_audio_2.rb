fc42a2b6c968c1ccdcecec355ef782c6fc0ada4e
James Cuzella <james.cuzella@lyraphase.com>
2015-06-11 20:43:09 -0600

    lyraphase_workstation::traktor_audio_2: Update Traktor Audio 2 Driver to 2.7.0

--- attributes/traktor_audio_2.rb
+++ attributes/traktor_audio_2.rb
@@ -1,7 +1,8 @@
-default['lyraphase_workstation']['traktor_audio_2']['dmg']['source']      = 'http://lyraphase.com/doc/installers/mac/Traktor_Audio_2_253_Mac_p.dmg'
-default['lyraphase_workstation']['traktor_audio_2']['dmg']['checksum']    = "39d4f45c4e9aaa32b882a39b175997173d07040094f5888c2e3ef72667753083"
+default['lyraphase_workstation']['traktor_audio_2']['dmg']['source']      = 'http://lyraphase.com/doc/installers/mac/Traktor_Audio_2_270_Mac_p.dmg'
+default['lyraphase_workstation']['traktor_audio_2']['dmg']['checksum']    = "e73d7ce1d023297e8081ae72c8cb23539efc18ed7c549df7ef5e19590c72d54e"
 default['lyraphase_workstation']['traktor_audio_2']['dmg']['volumes_dir'] = "Traktor Audio 2"
-default['lyraphase_workstation']['traktor_audio_2']['dmg']['dmg_name']    = 'TraktorAudio2_Mac'
-default['lyraphase_workstation']['traktor_audio_2']['dmg']['app']         = 'Traktor Audio 2 2.5.3 Installer Mac'
+default['lyraphase_workstation']['traktor_audio_2']['dmg']['dmg_name']    = 'Traktor_Audio_2_270_Mac_p'
+default['lyraphase_workstation']['traktor_audio_2']['dmg']['app']         = 'Traktor Audio 2 2.7.0 Installer Mac'
 default['lyraphase_workstation']['traktor_audio_2']['dmg']['type']        = 'pkg'
-default['lyraphase_workstation']['traktor_audio_2']['dmg']['package_id']  = 'com.caiaq.NIUSBTraktorAudio2Driver_10.6'
+# default['lyraphase_workstation']['traktor_audio_2']['dmg']['package_id']  = ''
+
