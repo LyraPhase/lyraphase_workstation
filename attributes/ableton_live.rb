af618d1eb1cb9c28b79c19c8b91d069c370daae1
James Cuzella <james.cuzella@lyraphase.com>
2015-06-14 15:56:51 -0600

    Adding Ableton Live installer recipe

---
+++ attributes/ableton_live.rb
@@ -0,0 +1,7 @@
+default['lyraphase_workstation']['ableton_live']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/ableton_live_suite_9.1.9_64.dmg'
+default['lyraphase_workstation']['ableton_live']['dmg']['checksum']    = "7a4da8531180afa8c7930a961289da1a0df567a8495e7ec205c7780f85dd0fda"
+default['lyraphase_workstation']['ableton_live']['dmg']['volumes_dir'] = "Ableton Live 9 Suite Installer"
+default['lyraphase_workstation']['ableton_live']['dmg']['dmg_name']    = 'ableton_live_suite_9.1.9_64'
+default['lyraphase_workstation']['ableton_live']['dmg']['app']         = 'Ableton Live 9 Suite'
+default['lyraphase_workstation']['ableton_live']['dmg']['type']        = 'app'
+# default['lyraphase_workstation']['ableton_live']['dmg']['package_id']  = ''
