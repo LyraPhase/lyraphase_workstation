c8d7ae64e01727aa54afe37a1f5852e96e0a80bf
James Cuzella <james.cuzella@lyraphase.com>
2015-06-14 12:59:37 -0600

    Adding Oxium 1.5.2 recipe

---
+++ attributes/oxium.rb
@@ -0,0 +1,7 @@
+default['lyraphase_workstation']['oxium']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/Oxium_full_osx_setup_V152.dmg'
+default['lyraphase_workstation']['oxium']['dmg']['checksum']    = "b124d9c42519f90fe8c576746cd5ef52c6c4ac2451724575c9dfff109ffa35b0"
+default['lyraphase_workstation']['oxium']['dmg']['volumes_dir'] = "Oxium 1.5.2"
+default['lyraphase_workstation']['oxium']['dmg']['dmg_name']    = 'Oxium_full_osx_setup_V152'
+default['lyraphase_workstation']['oxium']['dmg']['app']         = 'Oxium'
+default['lyraphase_workstation']['oxium']['dmg']['type']        = 'mpkg'
+default['lyraphase_workstation']['oxium']['dmg']['package_id']  = ''
