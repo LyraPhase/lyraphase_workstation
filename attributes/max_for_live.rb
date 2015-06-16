a590cd807bc91986571298ecbc0134ca07fa7182
James Cuzella <james.cuzella@lyraphase.com>
2015-06-16 01:56:43 -0600

    Adding Max for Live installer recipe

---
+++ attributes/max_for_live.rb
@@ -0,0 +1,7 @@
+default['lyraphase_workstation']['max_for_live']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/Max6110_150402.dmg'
+default['lyraphase_workstation']['max_for_live']['dmg']['checksum']    = "a974491ab16b66bab1e374c34c16ec66f0e8ca987b164a49ef92f3ff7aad453f"
+default['lyraphase_workstation']['max_for_live']['dmg']['volumes_dir'] = "Max61_150331_26bd7fb"
+default['lyraphase_workstation']['max_for_live']['dmg']['dmg_name']    = 'Max6110_150402'
+default['lyraphase_workstation']['max_for_live']['dmg']['app']         = 'Max 6.1 Installer'
+default['lyraphase_workstation']['max_for_live']['dmg']['type']        = 'pkg'
+# default['lyraphase_workstation']['max_for_live']['dmg']['package_id']  = ''
