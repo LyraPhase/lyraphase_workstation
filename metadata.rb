d7a0274fa1d9f0e01088fec2de537e04fd53df05
James Cuzella <james.cuzella@lyraphase.com>
2015-06-22 21:48:44 -0600

    Bump cookbook version to v0.3.5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "james.cuzella@lyraphase.com"
 license          "GNU Public License 3.0"
 description      "Recipes to Install & Configure my workstation"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.4"
+version          "0.3.5"
 
 supports         'mac_os_x'
 depends          'homebrew'
