65941a611258f048a97f52fb8f4b09b395873109
James Cuzella <james.cuzella@lyraphase.com>
2015-06-22 21:47:47 -0600

    Adding recipe for standard non-Live Cycling '74 Max 7.0.3

---
+++ attributes/cycling_74_max.rb
@@ -0,0 +1,7 @@
+default['lyraphase_workstation']['cycling74_max']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/Max703_150402.dmg'
+default['lyraphase_workstation']['cycling74_max']['dmg']['checksum']    = "c63616022a19e8aafe7c7036265599644b13f3ae640509ffe17a3c7c6953d55e"
+default['lyraphase_workstation']['cycling74_max']['dmg']['volumes_dir'] = "Max7_150409_37355d2"
+default['lyraphase_workstation']['cycling74_max']['dmg']['dmg_name']    = 'Max703_150402'
+default['lyraphase_workstation']['cycling74_max']['dmg']['app']         = 'Max'
+default['lyraphase_workstation']['cycling74_max']['dmg']['type']        = 'app'
+# default['lyraphase_workstation']['cycling74_max']['dmg']['package_id']  = 'com.cycling74.Max61.pkg'
---
+++ recipes/cycling_74_max.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: lyraphase_workstation
+# Recipe:: cycling74_max
+#
+# Copyright (C) 2015 James Cuzella
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+dmg_properties = node['lyraphase_workstation']['cycling74_max']['dmg']
+
+dmg_package "Max" do
+  source      dmg_properties['source']
+  checksum    dmg_properties['checksum']
+  volumes_dir dmg_properties['volumes_dir']
+  dmg_name    dmg_properties['dmg_name']
+  app         dmg_properties['app']
+  type        dmg_properties['type']
+  owner       node['current_user']
+  action :install
+end
