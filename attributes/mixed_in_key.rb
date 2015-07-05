4a6c0678f548ed3f5418df1df6d8fbaddaa47685
James Cuzella <james.cuzella@lyraphase.com>
2015-07-05 00:01:04 -0600

    Adding Mixed In Key 7.5.0.2327

---
+++ attributes/mixed_in_key.rb
@@ -0,0 +1,6 @@
+default['lyraphase_workstation']['mixed_in_key']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/Mixed+In+Key.7.5.0.2327.dmg'
+default['lyraphase_workstation']['mixed_in_key']['dmg']['checksum']    = '63c7ad86eb1871834142d78583fa846be0b012ba4010698b402f338289419ca0'
+default['lyraphase_workstation']['mixed_in_key']['dmg']['volumes_dir'] = 'Mixed In Key'
+default['lyraphase_workstation']['mixed_in_key']['dmg']['dmg_name']    = 'Mixed+In+Key.7.5.0.2327'
+default['lyraphase_workstation']['mixed_in_key']['dmg']['app']         = 'Mixed In Key 7'
+default['lyraphase_workstation']['mixed_in_key']['dmg']['type']        = 'app'
