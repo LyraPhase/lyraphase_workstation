93eadd06629e0e24c0a218098c207632d8415836
James Cuzella <james.cuzella@lyraphase.com>
2015-10-24 13:10:52 -0600

    Make ChefSpec tests compatible with multiple versions of chefspec gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
+
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
+  config.platform = 'mac_os_x'
+  config.version = '10.8.2' # FIXME: system ruby and fauxhai don't play nice
+                            # since there is no 10.9.2.json file yet. We
+                            # should submit a PR to fauxhai to fix this
+  config.before { stub_const('ENV', ENV.to_hash.merge('SUDO_USER' => 'brubble')) }
 end
--- spec/unit/nfs_mounts_spec.rb
+++ spec/unit/nfs_mounts_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe 'lyraphase_workstation::nfs_mounts' do
   let(:chef_run) {
-    ChefSpec::SoloRunner.new do |node|
+    klass = ChefSpec.constants.include?(:SoloRunner) ? ChefSpec::SoloRunner : ChefSpec::Runner
+    klass.new do |node|
       Struct.new("EtcPasswd", :name, :passwd, :uid, :gid, :gecos, :dir, :shell, :change, :uclass, :expire)
       node.set['etc']['passwd']['brubble'] = Struct::EtcPasswd.new('brubble', '********', 501, 20, 'Barney Rubble', '/Users/brubble', '/bin/bash', 0, '', 0)
       node.set['sprout']['user'] = 'brubble'
@@ -13,9 +14,6 @@ describe 'lyraphase_workstation::nfs_mounts' do
       ]
     end.converge(described_recipe)
   }
-  before do
-    stub_const('ENV', ENV.to_hash.merge('SUDO_USER' => 'fred'))
-  end
 
   [ '/etc/auto_nfs', '/etc/auto_master' ].each do |path|
 
