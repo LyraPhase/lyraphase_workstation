09d9cea1e8b6f6be5013fdf5033a72685bffd48a
James Cuzella <james.cuzella@lyraphase.com>
2015-11-18 17:57:35 -0700

    lyraphase_workstation::ableton_live: Attempt to fix nested hash NilClass indexing error

--- attributes/ableton_live.rb
+++ attributes/ableton_live.rb
@@ -1,3 +1,4 @@
+default['lyraphase_workstation']['ableton_live'] = {}
 default['lyraphase_workstation']['ableton_live']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/ableton_live_suite_9.1.9_64.dmg'
 default['lyraphase_workstation']['ableton_live']['dmg']['checksum']    = "7a4da8531180afa8c7930a961289da1a0df567a8495e7ec205c7780f85dd0fda"
 default['lyraphase_workstation']['ableton_live']['dmg']['volumes_dir'] = "Ableton Live 9 Suite Installer"
---
+++ attributes/default.rb
@@ -0,0 +1,1 @@
+default['lyraphase_workstation'] = {}
