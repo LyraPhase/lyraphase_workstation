422ab67c57ba588d889a4d57f9002228deba11ed
James Cuzella <james.cuzella@lyraphase.com>
2016-01-03 13:25:42 -0700

    Update I Wish plugin to v1

--- attributes/polyverse_infected_mushroom_i_wish.rb
+++ attributes/polyverse_infected_mushroom_i_wish.rb
@@ -1,8 +1,8 @@
 default['lyraphase_workstation']['polyverse_infected_mushroom_i_wish'] = {}
-default['lyraphase_workstation']['polyverse_infected_mushroom_i_wish']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/Infected Mushroom - I Wish v0.910.dmg'
-default['lyraphase_workstation']['polyverse_infected_mushroom_i_wish']['dmg']['checksum']    = "d63dbfde7554b5c3129992bb70bd88d28b4a6b27f0ca763fb9a7245617138390"
-default['lyraphase_workstation']['polyverse_infected_mushroom_i_wish']['dmg']['volumes_dir'] = "Infected Mushroom - I wish 0.9"
-default['lyraphase_workstation']['polyverse_infected_mushroom_i_wish']['dmg']['dmg_name']    = 'Infected Mushroom - I Wish v0.910'
+default['lyraphase_workstation']['polyverse_infected_mushroom_i_wish']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/Infected Mushroom - I Wish v1.dmg'
+default['lyraphase_workstation']['polyverse_infected_mushroom_i_wish']['dmg']['checksum']    = "cadda4e59475efe0d39598622a769826a23e3286d47f1a5ea3bda8248ea84423"
+default['lyraphase_workstation']['polyverse_infected_mushroom_i_wish']['dmg']['volumes_dir'] = "Infected Mushroom - I Wish v1"
+default['lyraphase_workstation']['polyverse_infected_mushroom_i_wish']['dmg']['dmg_name']    = 'Infected Mushroom - I Wish v1'
 default['lyraphase_workstation']['polyverse_infected_mushroom_i_wish']['dmg']['app']         = 'I Wish Setup'
 default['lyraphase_workstation']['polyverse_infected_mushroom_i_wish']['dmg']['package_id']  = 'com.polyversemusic.iWishSetup.IWish64.pkg'
 default['lyraphase_workstation']['polyverse_infected_mushroom_i_wish']['dmg']['type']        = 'pkg'
