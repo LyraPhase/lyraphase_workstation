9801deec78e2b96c62c4c284d3d2c81d325eea6b
James Cuzella <james.cuzella@lyraphase.com>
2016-10-02 22:53:09 -0600

    Force default airfoil attributes to nil

---
+++ attributes/airfoil.rb
@@ -0,0 +1,5 @@
+default['lyraphase_workstation']['airfoil'] = {}
+default['lyraphase_workstation']['airfoil']['plist_file'] = nil
+default['lyraphase_workstation']['airfoil']['license'] = {}
+default['lyraphase_workstation']['airfoil']['license']['name'] = nil
+default['lyraphase_workstation']['airfoil']['license']['code'] = nil
