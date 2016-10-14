17e88e3375e97dd4818971e7891f987e7367151d
James Cuzella <james.cuzella@lyraphase.com>
2016-10-13 18:32:38 -0600

    Initial OSX natural scrolling direction recipe

---
+++ attributes/osx_natural_scrolling.rb
@@ -0,0 +1,8 @@
+# Default natural scrolling to "on"
+# Natural Scrolling on: Scrolling up/down behaves as if you are swiping a piece of paper in the physical world
+#   fingers up = page down
+#   fingers down = page up
+# Natural Scrolling off: Scrolling up/down is exactly the same as the direction you are moving your fingers
+#   fingers up = page up
+#   fingers down = page down
+default['lyraphase_workstation']['settings']['natural_scrolling'] = true
---
+++ recipes/osx_natural_scrolling.rb
@@ -0,0 +1,5 @@
+osx_defaults "Disable natural scrolling" do
+  domain "/Users/#{node['sprout']['user']}/Library/Preferences/.GlobalPreferences"
+  key "com.apple.swipescrolldirection"
+  boolean node['lyraphase_workstation']['settings']['natural_scrolling']
+end
