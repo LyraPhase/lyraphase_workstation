57bc01d1ac7824048a3396b76216c3ce932c897d
James Cuzella <james.cuzella@lyraphase.com>
2016-10-15 13:33:44 -0600

    Update Ableton Suite to 9.7 stable

--- attributes/ableton_live.rb
+++ attributes/ableton_live.rb
@@ -1,7 +1,7 @@
 default['lyraphase_workstation']['ableton_live'] = {}
-default['lyraphase_workstation']['ableton_live']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/ableton_live_suite_9.5_64.dmg'
-default['lyraphase_workstation']['ableton_live']['dmg']['checksum']    = "c3fc7fbd79fb30a6d4f2b924f2c1015438d686c2fb3f2bf23f362dfd4244f072"
+default['lyraphase_workstation']['ableton_live']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/ableton_live_suite_9.7_64.dmg'
+default['lyraphase_workstation']['ableton_live']['dmg']['checksum']    = "35fa16a2c703d458fc005413e81e7fccd3ebf18eff5bffea91eb9165db42c400"
 default['lyraphase_workstation']['ableton_live']['dmg']['volumes_dir'] = "Ableton Live 9 Suite Installer"
-default['lyraphase_workstation']['ableton_live']['dmg']['dmg_name']    = 'ableton_live_suite_9.5_64'
+default['lyraphase_workstation']['ableton_live']['dmg']['dmg_name']    = 'ableton_live_suite_9.7_64'
 default['lyraphase_workstation']['ableton_live']['dmg']['app']         = 'Ableton Live 9 Suite'
 default['lyraphase_workstation']['ableton_live']['dmg']['type']        = 'app'
