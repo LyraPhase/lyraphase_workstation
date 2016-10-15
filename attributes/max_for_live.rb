e24ba5391a06bbf25d550a726d056677cf22a037
James Cuzella <james.cuzella@lyraphase.com>
2016-10-15 13:34:17 -0600

    Update Max 7 to 7.3.0

--- attributes/max_for_live.rb
+++ attributes/max_for_live.rb
@@ -1,7 +1,7 @@
-default['lyraphase_workstation']['max_for_live']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/Max6110_150402.dmg'
-default['lyraphase_workstation']['max_for_live']['dmg']['checksum']    = "a974491ab16b66bab1e374c34c16ec66f0e8ca987b164a49ef92f3ff7aad453f"
-default['lyraphase_workstation']['max_for_live']['dmg']['volumes_dir'] = "Max61_150331_26bd7fb"
-default['lyraphase_workstation']['max_for_live']['dmg']['dmg_name']    = 'Max6110_150402'
-default['lyraphase_workstation']['max_for_live']['dmg']['app']         = 'Max 6.1 Installer'
-default['lyraphase_workstation']['max_for_live']['dmg']['type']        = 'pkg'
-default['lyraphase_workstation']['max_for_live']['dmg']['package_id']  = 'com.cycling74.Max61.pkg'
+default['lyraphase_workstation']['max_for_live']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/Max731_161019.dmg'
+default['lyraphase_workstation']['max_for_live']['dmg']['checksum']    = 'a03ea117c8d40bbc203188d114dc22d0abe4c96281c6d5afa3e1037fa1339af1'
+default['lyraphase_workstation']['max_for_live']['dmg']['volumes_dir'] = 'Max7_161017_340a77a'
+default['lyraphase_workstation']['max_for_live']['dmg']['dmg_name']    = 'Max731_161019'
+default['lyraphase_workstation']['max_for_live']['dmg']['app']         = 'Max'
+default['lyraphase_workstation']['max_for_live']['dmg']['type']        = 'app'
+# default['lyraphase_workstation']['max_for_live']['dmg']['package_id']  = 'com.cycling74.Max61.pkg'
