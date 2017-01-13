933991e8d1f93fa48821449253111868c8d331b9
James Cuzella <james.cuzella@lyraphase.com>
2017-01-12 22:37:07 -0700

    Fix redefinition of ableton_live attribute key

--- attributes/ableton_live_options.rb
+++ attributes/ableton_live_options.rb
@@ -1,4 +1,3 @@
-default['lyraphase_workstation']['ableton_live'] = {}
 default['lyraphase_workstation']['ableton_live']['options'] = []
 # Default to managing Options.txt of latest detected Ableton version
 default['lyraphase_workstation']['ableton_live']['managed_versions'] = nil
