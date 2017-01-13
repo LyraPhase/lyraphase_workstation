a31d5c8e977a9983d83c00bcf8b1b0bcd16e3695
James Cuzella <james.cuzella@lyraphase.com>
2017-01-12 22:40:48 -0700

    Add ChefSpec tests for StreamLocalBindUnlink, disable gpgtools LaunchAgent

--- spec/unit/recipes/gpg21_spec.rb
+++ spec/unit/recipes/gpg21_spec.rb
@@ -17,6 +17,47 @@
 
 require 'spec_helper'
 
+content = <<EOPLIST
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>KeepAlive</key>
+  <false/>
+  <key>Label</key>
+  <string>org.gpgtools.macgpg2.fix</string>
+  <key>ProgramArguments</key>
+  <array>
+    <string>/usr/local/MacGPG2/libexec/fixGpgHome</string>
+  </array>
+  <key>RunAtLoad</key>
+  <true/>
+</dict>
+</plist>
+
+EOPLIST
+
+updated_content = <<EOPLIST
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>KeepAlive</key>
+  <false/>
+  <key>Label</key>
+  <string>org.gpgtools.macgpg2.fix</string>
+  <key>ProgramArguments</key>
+  <array>
+    <string>/usr/local/MacGPG2/libexec/fixGpgHome</string>
+  </array>
+  <key>RunAtLoad</key>
+  <false/>
+</dict>
+</plist>
+
+EOPLIST
+
+test_file = Pathname.new(Dir.tmpdir) + "plist_file_push_spec.plist"
 
 describe 'lyraphase_workstation::gpg21' do
 
@@ -30,8 +71,19 @@ describe 'lyraphase_workstation::gpg21' do
       node.normal['etc']['passwd']['brubble'] = Struct::EtcPasswd.new('brubble', '********', 501, 20, 'Barney Rubble', '/Users/brubble', '/bin/bash', 0, '', 0)
       node.normal['lyraphase_workstation']['user'] = 'brubble'
       node.normal['lyraphase_workstation']['home'] = '/Users/brubble'
+
+      node.normal['lyraphase_workstation']['gpg21']['gpgtools_plist_file'] = test_file
     end.converge(described_recipe)
   }
+
+  before(:all) do
+    test_file.open("wb") { |f| f.write(content) }
+
+    # ChefSpec::ServerRunner.new(step_into: ["plist_file"]) do |node|
+    #   node.set['lyraphase_workstation']['airfoil']['plist_file'] = test_file
+    # end.converge(described_recipe)
+
+  end
 
   it 'taps homebrew/versions' do
     expect(chef_run).to tap_homebrew_tap('homebrew/versions')
@@ -58,6 +110,34 @@ describe 'lyraphase_workstation::gpg21' do
     )
     expect(chef_run).to render_file(launchd_plist).with_content(Regexp.new("^\\s+<string>#{Regexp.escape(fixGpgHome_script_path)}</string>$"))
     expect(chef_run).to render_file(launchd_plist).with_content(Regexp.new("^\\s+<string>#{Regexp.escape(File.basename(launchd_plist).gsub(/\.plist$/, ''))}</string>$"))
+  end
+
+  let(:sshd_config_file) { instance_double('Chef::Util::FileEdit') }
+  let(:dummy_class) { Chef::Util::FileEdit }
+  before do
+    allow_any_instance_of(dummy_class).to receive(:new).with('/etc/ssh/sshd_config') { sshd_config_file }
+    # allow(dummy_class).to receive(:new).with('/etc/ssh/sshd_config') { sshd_config_file }
+    allow(sshd_config_file).to receive(:insert_line_if_no_match).with("/^# Remove stale forwarding sockets (https://wiki.gnupg.org/AgentForwarding)/", "# Remove stale forwarding sockets (https://wiki.gnupg.org/AgentForwarding)")
+    allow(sshd_config_file).to receive(:insert_line_if_no_match).with("/^StreamLocalBindUnlink.*/", "StreamLocalBindUnlink yes")
+    # allow(sshd_config_file).to receive(:write_file) {}
+    # RSpec 2 syntax
+    # Chef::Util::FileEdit.stub(:new).with('/etc/ssh/sshd_config').and_return(sshd_config_file)
+  end
+
+  it "disables the gpgtools launchd plist file" do
+    # File.open('/tmp/chefspec.plist', 'w') do |f|
+    #   f.write(test_file.open("rb") { |f| f.read })
+    # end
+    # expect(test_file.open("rb") { |f| f.read }).to eq(updated_content)
+    expect(chef_run).to update_plist_file(test_file)
+  end
+
+  it 'adds StreamLocalBindUnlink to sshd config' do
+    expect(dummy_class.new('/etc/ssh/sshd_config')).to eq(sshd_config_file)
+    # Chef::Util::FileEdit.should_receive(:new).with('/etc/ssh/sshd_config').and_return(sshd_config_file)
+    # sshd_config_file.should_receive(:insert_line_if_no_match).with('example.com', '1.1.1.1 2.2.2.2')
+    # sshd_config_file.should_receive(:write_file)
+    expect(sshd_config_file).to be_an_instance_of?(Chef::Util::FileEdit)
   end
 
 end
