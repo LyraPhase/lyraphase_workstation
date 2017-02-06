d288ec9b3278e5fa86df6006389d5b6231f76e4b
James Cuzella <james.cuzella@lyraphase.com>
2017-02-05 18:49:18 -0700

    lyraphase_workstation::ableton_live: Remove debug statements

--- attributes/ableton_live.rb
+++ attributes/ableton_live.rb
@@ -6,6 +6,3 @@ default['lyraphase_workstation']['ableton_live']['dmg']['volumes_dir'] = "Ableto
 default['lyraphase_workstation']['ableton_live']['dmg']['dmg_name']    = 'ableton_live_suite_9.7_64'
 default['lyraphase_workstation']['ableton_live']['dmg']['app']         = 'Ableton Live 9 Suite'
 default['lyraphase_workstation']['ableton_live']['dmg']['type']        = 'app'
-Chef::Log.warn("INSIDE COOKBOOK ATTRIBUTES FILE: Loaded Attributes for: lyraphase_workstation::ableton_live Recipe")
-Chef::Log.warn("INSIDE COOKBOOK ATTRIBUTES FILE: is node['lyraphase_workstation']['ableton_live']['dmg'] a kind of Hash? #{node['lyraphase_workstation']['ableton_live']['dmg'].kind_of?(Hash)}")
-Chef::Log.warn("INSIDE COOKBOOK ATTRIBUTES FILE: Class of node['lyraphase_workstation']['ableton_live']['dmg'] is: #{node['lyraphase_workstation']['ableton_live']['dmg'].class}")
