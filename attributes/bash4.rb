36bf6be5e5b81e0e4e55205e650564c52dd6d8a4
James Cuzella <james.cuzella@lyraphase.com>
2017-04-07 10:30:56 -0600

    Add recipe for bash v4 + bash-completion@2

---
+++ attributes/bash4.rb
@@ -0,0 +1,12 @@
+default['lyraphase_workstation']['bash'] = {}
+default['lyraphase_workstation']['bash']['etc_shells_path'] = '/etc/shells'
+default['lyraphase_workstation']['bash']['etc_shells'] =
+                                            [
+                                              '/usr/local/bin/bash',
+                                              '/bin/bash',
+                                              '/bin/csh',
+                                              '/bin/ksh',
+                                              '/bin/sh',
+                                              '/bin/tcsh',
+                                              '/bin/zsh'
+                                            ]
