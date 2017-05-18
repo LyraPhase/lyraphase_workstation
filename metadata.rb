3d4963679b72b396d26eb8bd04f5fbcfbd666173
James Cuzella <james.cuzella@lyraphase.com>
2017-05-18 16:16:59 -0600

    Fix FC066: Ensure chef_version is set in metadata

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license          "GNU Public License 3.0"
 description      "Recipes to Install & Configure my workstation"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.2.3"
+chef_version     ">= 12.0"
 
 source_url 'https://github.com/trinitronx/lyraphase_workstation' if respond_to?(:source_url)
 issues_url 'https://github.com/trinitronx/lyraphase_workstation/issues' if respond_to?(:issues_url)
