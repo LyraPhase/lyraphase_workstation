4f98cefc1f781f9d1f144e1030ac35a6abf3ba03
James Cuzella <james.cuzella@lyraphase.com>
2018-02-06 20:11:07 -0700

    Fix FC069, FC078: use SPDX.org defined OSI license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "lyraphase_workstation"
 maintainer       "James Cuzella"
 maintainer_email "james.cuzella@lyraphase.com"
-license          "GNU Public License 3.0"
+license          "GPL-3.0+"
 description      "Recipes to Install & Configure my workstation"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.5.0"
