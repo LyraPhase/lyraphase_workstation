079d7fd2a6615019f8be7051e0db76600f4cc49c
James Cuzella <james.cuzella@lyraphase.com>
2018-05-03 15:59:38 -0600

    Add initial recipe lyraphase_workstation::bashrc from lyra .bashrc file

---
+++ attributes/bashrc.rb
@@ -0,0 +1,4 @@
+default['lyraphase_workstation']['bashrc'] = {}
+default['lyraphase_workstation']['bashrc']['user_fullname'] = 'James Cuzella'
+default['lyraphase_workstation']['bashrc']['user_email'] = 'james.cuzella@lyraphase.com'
+default['lyraphase_workstation']['bashrc']['user_gpg_keyid'] = '0x2689A459B1568D09'
