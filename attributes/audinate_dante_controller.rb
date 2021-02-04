185ae299baa531d123b46a6c9e6c97e173aeb891
James Cuzella <james.cuzella@lyraphase.com>
2021-02-03 22:48:43 -0700

    Adding new recipe: lyraphase_workstation::audinate_dante_controller

---
+++ attributes/audinate_dante_controller.rb
@@ -0,0 +1,9 @@
+default['lyraphase_workstation']['audinate_dante_controller']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/DanteController-4.2.7.1_macos.dmg'
+default['lyraphase_workstation']['audinate_dante_controller']['dmg']['checksum']    = 'e601346160478447ccd572810ae01343e4beb76644554f4ed62d707c733a3547'
+default['lyraphase_workstation']['audinate_dante_controller']['dmg']['volumes_dir'] = 'Dante Controller'
+default['lyraphase_workstation']['audinate_dante_controller']['dmg']['dmg_name']    = 'DanteController-4.2.7.1_macos'
+default['lyraphase_workstation']['audinate_dante_controller']['dmg']['app']         = 'DanteController'
+default['lyraphase_workstation']['audinate_dante_controller']['dmg']['type']        = 'pkg'
+# default['lyraphase_workstation']['audinate_dante_controller']['dmg']['pkg_file']    = 'DanteController.pkg'
+# TODO: Find real pkg ID after 1st install
+# default['lyraphase_workstation']['audinate_dante_controller']['dmg']['package_id']  = 'com.audinate.DanteController.pkg'
