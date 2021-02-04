8bc703b48d8ed05a9cd1c156a9cd93a310474ff3
James Cuzella <james.cuzella@lyraphase.com>
2021-02-03 19:48:25 -0700

    Update machine_name recipe from pivotal-sprout/sprout-osx-settings@62630be

--- recipes/machine_name.rb
+++ recipes/machine_name.rb
@@ -1,45 +1,54 @@
-require 'mixlib/shellout'
+def hostname
+  cmd = Mixlib::ShellOut.new('hostname')
+  cmd.run_command
+  cmd.stdout.chomp
+end
 
-hostname = Mixlib::ShellOut.new('hostname')
-hostname.run_command
+def real_interfaces
+  cmd = Mixlib::ShellOut.new('netstat -ni')
+  cmd.run_command
+  cmd.stdout.split("\n").select { |line| line.match(/en.*((\d+\.){3}\d+)/) }
+end
 
-hostnames=[hostname.stdout.chop]
+hostnames = [hostname]
 
 require 'socket'
-netstat = Mixlib::ShellOut.new('netstat -ni')
-netstat.run_command
-real_interfaces = netstat.stdout.split("\n").select {|line| line.match(/en.*((\d+\.){3}\d+)/) }
-host_ips = real_interfaces.collect {|line| line.match(/en.*?((\d+\.){3}\d+)/); Regexp.last_match(1) }
+host_ips = real_interfaces.collect do |line|
+  line =~ /en.*?((\d+\.){3}\d+)/
+  Regexp.last_match(1)
+end
+
 host_ips.each do |ip|
   begin
-    hostnames << Socket.gethostbyaddr(ip.split(/\./).collect! {|i| i.to_i }.pack('CCCC'))[0]
+    hostnames << Socket.gethostbyaddr(ip.split(/\./).collect!(&:to_i).pack('CCCC'))[0]
   rescue SocketError
     log "no reverse lookup for \"#{ip}\""
   end
 end
 
-hostnames.each do |hostname|
-  puts "My hostname: #{hostname}"
-  if hostname =~ /#{node['machine_domain']}/ and hostname !~ /^dyn-/
-    hostname = hostname.gsub(/\..*/,"")
+hostnames.uniq.each do |hostname|
+  log "My hostname: #{hostname}"
+  next unless hostname =~ /#{node['machine_domain']}/ && hostname !~ /^dyn-/
+  hostname = hostname.gsub(/\..*/, '')
 
-    # The scutil commands need to run as root, unless
-    # you're logged into the console, but we can't be sure of that.
+  # The scutil commands need to run as root, unless
+  # you're logged into the console, but we can't be sure of that.
 
-    ["scutil --set ComputerName #{hostname}",
-     "scutil --set LocalHostName #{hostname}",
-     "scutil --set HostName #{hostname}",
-     "hostname #{hostname}",
-     "diskutil rename / #{hostname}" ].each do |host_cmd|
-      puts host_cmd
-    end
+  ["scutil --set ComputerName #{hostname}",
+   "scutil --set LocalHostName #{hostname}",
+   "scutil --set HostName #{hostname}",
+   "hostname #{hostname}",
+   "diskutil rename / #{hostname}"].each do |host_cmd|
+    execute host_cmd
+  end
 
-    ruby_block "test to see if hostname was set" do
-      block do
-        scutil_computer_name = Mixlib::ShellOut.new('scutil --get ComputerName')
-        scutil_computer_name.run_command
-        raise "Setting of hostname failed" unless hostname == scutil_computer_name.stdout.chop
-      end
+  ruby_block "test to see if hostname (#{hostname}) was set" do
+    block do
+      cmd = Mixlib::ShellOut.new('scutil --get ComputerName')
+      cmd.run_command
+      computer_name = cmd.stdout.chomp
+
+      raise 'Setting of hostname failed' unless hostname == computer_name
     end
   end
 end
