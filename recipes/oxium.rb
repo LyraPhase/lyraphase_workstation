dd7372c615fcf1acd15974220d10ead2f9d230fa
James Cuzella <james.cuzella@lyraphase.com>
2021-12-13 22:13:35 -0700

    CookStyle fixups for oxium

--- recipes/oxium.rb
+++ recipes/oxium.rb
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
+# frozen_string_literal: true
+
 #
-# Cookbook Name:: lyraphase_workstation
+# Cookbook:: lyraphase_workstation
 # Recipe:: oxium
+# Site:: https://www.xils-lab.com/products/oxium-p-141.html
 #
-# Copyright (C) © 🄯  2013-2020 James Cuzella
-# 
+# License:: GPL-3.0+
+# Copyright:: (C) © 🄯  2013-2021 James Cuzella
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -19,7 +24,7 @@
 #
 dmg_properties = node['lyraphase_workstation']['oxium']['dmg']
 
-dmg_package "Oxium" do
+dmg_package 'Oxium' do
   source      dmg_properties['source']
   checksum    dmg_properties['checksum']
   volumes_dir dmg_properties['volumes_dir']
