763426b0b681dc45fe8c7367be9813f5faae0b36
James Cuzella <james.cuzella@lyraphase.com>
2021-12-13 21:09:04 -0700

    CookStyle fixups for gpg21

--- attributes/gpg21.rb
+++ attributes/gpg21.rb
@@ -1,21 +1,26 @@
+# -*- coding: utf-8 -*-
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/LineLength
 default['lyraphase_workstation']['gpg21'] = {}
 default['lyraphase_workstation']['gpg21']['gpgtools_plist_file'] = '/Library/LaunchAgents/org.gpgtools.macgpg2.fix.plist'
 default['lyraphase_workstation']['gpg21']['binary_paths'] =
-                                            [
-                                              '/usr/local/bin/addgnupghome',
-                                              '/usr/local/bin/applygnupgdefaults',
-                                              '/usr/local/bin/dirmngr',
-                                              '/usr/local/bin/dirmngr-client',
-                                              '/usr/local/bin/gpg-agent',
-                                              '/usr/local/bin/gpg-connect-agent',
-                                              '/usr/local/bin/gpg2',
-                                              '/usr/local/bin/gpgconf',
-                                              '/usr/local/bin/gpgparsemail',
-                                              '/usr/local/bin/gpgscm',
-                                              '/usr/local/bin/gpgsm',
-                                              '/usr/local/bin/gpgtar',
-                                              '/usr/local/bin/gpgv2',
-                                              '/usr/local/bin/kbxutil',
-                                              '/usr/local/bin/symcryptrun',
-                                              '/usr/local/bin/watchgnupg'
-                                            ]
+  [
+    '/usr/local/bin/addgnupghome',
+    '/usr/local/bin/applygnupgdefaults',
+    '/usr/local/bin/dirmngr',
+    '/usr/local/bin/dirmngr-client',
+    '/usr/local/bin/gpg-agent',
+    '/usr/local/bin/gpg-connect-agent',
+    '/usr/local/bin/gpg2',
+    '/usr/local/bin/gpgconf',
+    '/usr/local/bin/gpgparsemail',
+    '/usr/local/bin/gpgscm',
+    '/usr/local/bin/gpgsm',
+    '/usr/local/bin/gpgtar',
+    '/usr/local/bin/gpgv2',
+    '/usr/local/bin/kbxutil',
+    '/usr/local/bin/symcryptrun',
+    '/usr/local/bin/watchgnupg'
+  ]
+# rubocop:enable Metrics/LineLength
