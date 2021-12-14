0ae4f3728e9787add20250ae1c13d22c766f5fdf
James Cuzella <james.cuzella@lyraphase.com>
2021-12-13 19:04:18 -0700

    CookStyle fixups for polyverse_infected_mushroom_manipulator

--- attributes/polyverse_infected_mushroom_manipulator.rb
+++ attributes/polyverse_infected_mushroom_manipulator.rb
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
+# frozen_string_literal: true
+
 default['lyraphase_workstation']['polyverse_infected_mushroom_manipulator'] = {}
 default['lyraphase_workstation']['polyverse_infected_mushroom_manipulator']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/Infected Mushroom - Manipulator v1.01.dmg'
-default['lyraphase_workstation']['polyverse_infected_mushroom_manipulator']['dmg']['checksum']    = "d1b16dfd199b6a69692f7901edc4546dbf3f43e927bc08e14c5d1c83b5798e95"
-default['lyraphase_workstation']['polyverse_infected_mushroom_manipulator']['dmg']['volumes_dir'] = "Manipulator Setup"
+default['lyraphase_workstation']['polyverse_infected_mushroom_manipulator']['dmg']['checksum']    = 'd1b16dfd199b6a69692f7901edc4546dbf3f43e927bc08e14c5d1c83b5798e95'
+default['lyraphase_workstation']['polyverse_infected_mushroom_manipulator']['dmg']['volumes_dir'] = 'Manipulator Setup'
 default['lyraphase_workstation']['polyverse_infected_mushroom_manipulator']['dmg']['dmg_name']    = 'Infected Mushroom - Manipulator v1.01'
 default['lyraphase_workstation']['polyverse_infected_mushroom_manipulator']['dmg']['app']         = 'Manipulator Setup'
 default['lyraphase_workstation']['polyverse_infected_mushroom_manipulator']['dmg']['package_id']  = 'com.polyversemusic.manipulatorSetup.Manipulator.pkg'
