6d411ef870d5b36ae27037237216e7083787f0ad
James Cuzella <james.cuzella@lyraphase.com>
2021-12-13 18:02:05 -0700

    lyraphase_workstation::root_bootstrap_ssh_config: Add ChefSpec tests for quantum superposition of root ssh file existence

--- spec/unit/recipes/root_bootstrap_ssh_config_spec.rb
+++ spec/unit/recipes/root_bootstrap_ssh_config_spec.rb
@@ -24,61 +24,110 @@ require 'spec_helper'
 
 describe 'lyraphase_workstation::root_bootstrap_ssh_config' do
 
-  let(:root_ssh) { '/var/root/.ssh' }
-  let(:root_ssh_config) { '/var/root/.ssh/config' }
-  let(:root_ssh_known_hosts) { '/var/root/.ssh/known_hosts' }
-  let(:chef_run) {
-    klass = ChefSpec.constants.include?(:SoloRunner) ? ChefSpec::SoloRunner : ChefSpec::Runner
-    klass.new do |node|
-      create_singleton_struct "EtcPasswd", [ :name, :passwd, :uid, :gid, :gecos, :dir, :shell, :change, :uclass, :expire ]
-      node.normal['etc']['passwd']['brubble'] = Struct::EtcPasswd.new('brubble', '********', 501, 20, 'Barney Rubble', '/Users/brubble', '/bin/bash', 0, '', 0)
-      node.normal['lyraphase_workstation']['user'] = 'brubble'
+  context 'when root .ssh/* files do not exist' do
+    let(:root_ssh) { '/var/root/.ssh' }
+    let(:root_ssh_config) { '/var/root/.ssh/config' }
+    let(:root_ssh_known_hosts) { '/var/root/.ssh/known_hosts' }
+    let(:chef_run) {
+      klass = ChefSpec.constants.include?(:SoloRunner) ? ChefSpec::SoloRunner : ChefSpec::Runner
+      klass.new do |node|
+        create_singleton_struct "EtcPasswd", [ :name, :passwd, :uid, :gid, :gecos, :dir, :shell, :change, :uclass, :expire ]
+        node.normal['etc']['passwd']['brubble'] = Struct::EtcPasswd.new('brubble', '********', 501, 20, 'Barney Rubble', '/Users/brubble', '/bin/bash', 0, '', 0)
+        node.normal['lyraphase_workstation']['user'] = 'brubble'
 
-      node.normal['lyraphase_workstation']['root_bootstrap_ssh_config']['identity_file'] = 'identity.brubble'
-    end.converge(described_recipe)
-  }
+        node.normal['lyraphase_workstation']['root_bootstrap_ssh_config']['identity_file'] = 'identity.brubble'
+        stub_command('grep -q github.com known_hosts').and_return(false)
+      end.converge(described_recipe)
+    }
 
-  before :all do
-    expect(File).not_to be_exists('/var/root/.ssh')
-    expect(File).not_to be_exists('/var/root/.ssh/config')
-    expect(File).not_to be_exists('/var/root/.ssh/known_hosts')
+    before :each do
+      allow(File).to receive(:exists?).and_call_original
+      allow(File).to receive(:exists?).with(anything).and_call_original
+      allow(File).to receive(:exists?).with('/var/root/.ssh').and_return(false)
+      allow(File).to receive(:exists?).with('/var/root/.ssh/config').and_return(false)
+      allow(File).to receive(:exists?).with('/var/root/.ssh/known_hosts').and_return(false)
+      expect(File).not_to be_exists('/var/root/.ssh')
+      expect(File).not_to be_exists('/var/root/.ssh/config')
+      expect(File).not_to be_exists('/var/root/.ssh/known_hosts')
+    end
+
+    it 'creates directory /var/root/.ssh' do
+      expect(chef_run).to create_directory(root_ssh).with(
+        owner: 'root',
+        group: 'wheel',
+        mode: '0700'
+      )
+    end
+
+    it 'creates known_hosts file if missing' do
+      expect(chef_run).to create_file_if_missing(root_ssh_known_hosts).with(
+        owner: 'root',
+        group: 'wheel',
+        mode: '0644'
+      )
+    end
+
+    it 'adds github rsa keys to known_hosts' do
+      expect(chef_run).to run_execute('add github to known_hosts').with(user: 'root')
+    end
+
+    it 'creates a template for /var/root/.ssh/config' do
+      expect(chef_run).to create_template(root_ssh_config).with(
+        user: 'root',
+        group: 'wheel',
+        mode: '0600'
+      )
+    end
+
+    it 'creates minimal root user github.com ssh config' do
+      expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+IdentityFile\s+\/Users\/brubble\/.ssh\/identity.brubble$/)
+      expect(chef_run).to render_file(root_ssh_config).with_content(/^Host github\.com$/)
+      expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+User\s+git$/)
+      expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+Hostname\s+github\.com$/)
+      expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+PreferredAuthentications\s+publickey$/)
+      expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+StrictHostKeyChecking\s+no$/)
+      expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+UserKnownHostsFile\s+\/dev\/null$/)
+    end
   end
 
-  it 'creates directory /var/root/.ssh' do
-    expect(chef_run).to create_directory(root_ssh).with(
-      owner: 'root',
-      group: 'wheel',
-      mode: '0700'
-    )
-  end
 
-  it 'creates known_hosts file if missing' do
-    expect(chef_run).to create_file_if_missing(root_ssh_known_hosts).with(
-      owner: 'root',
-      group: 'wheel',
-      mode: '0644'
-    )
-  end
+  context 'when root .ssh/* files already exist and github.com already in known_hosts ...' do
+    let(:root_ssh) { '/var/root/.ssh' }
+    let(:root_ssh_config) { '/var/root/.ssh/config' }
+    let(:root_ssh_known_hosts) { '/var/root/.ssh/known_hosts' }
+    let(:chef_run) {
+      klass = ChefSpec.constants.include?(:SoloRunner) ? ChefSpec::SoloRunner : ChefSpec::Runner
+      klass.new do |node|
+        create_singleton_struct "EtcPasswd", [ :name, :passwd, :uid, :gid, :gecos, :dir, :shell, :change, :uclass, :expire ]
+        node.normal['etc']['passwd']['brubble'] = Struct::EtcPasswd.new('brubble', '********', 501, 20, 'Barney Rubble', '/Users/brubble', '/bin/bash', 0, '', 0)
+        node.normal['lyraphase_workstation']['user'] = 'brubble'
 
-  it 'adds github rsa keys to known_hosts' do
-    expect(chef_run).to run_execute('add github to known_hosts').with(user: 'root')
-  end
+        node.normal['lyraphase_workstation']['root_bootstrap_ssh_config']['identity_file'] = 'identity.brubble'
+        stub_command('grep -q github.com known_hosts').and_return(true)
+      end.converge(described_recipe)
+    }
 
-  it 'creates a template for /var/root/.ssh/config' do
-    expect(chef_run).to create_template(root_ssh_config).with(
-      user: 'root',
-      group: 'wheel',
-      mode: '0600'
-    )
-  end
+    before :each do
+      allow(File).to receive(:exists?).and_call_original
+      allow(File).to receive(:exists?).with(anything).and_call_original
+      allow(File).to receive(:exists?).with('/var/root/.ssh').and_return(true)
+      allow(File).to receive(:exists?).with('/var/root/.ssh/config').and_return(true)
+      allow(File).to receive(:exists?).with('/var/root/.ssh/known_hosts').and_return(true)
+      expect(File).to be_exists('/var/root/.ssh')
+      expect(File).to be_exists('/var/root/.ssh/config')
+      expect(File).to be_exists('/var/root/.ssh/known_hosts')
+    end
 
-  it 'creates minimal root user github.com ssh config' do
-    expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+IdentityFile\s+\/Users\/brubble\/.ssh\/identity.brubble$/)
-    expect(chef_run).to render_file(root_ssh_config).with_content(/^Host github\.com$/)
-    expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+User\s+git$/)
-    expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+Hostname\s+github\.com$/)
-    expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+PreferredAuthentications\s+publickey$/)
-    expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+StrictHostKeyChecking\s+no$/)
-    expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+UserKnownHostsFile\s+\/dev\/null$/)
+    it 'creates known_hosts file if missing' do
+      expect(chef_run).to create_file_if_missing(root_ssh_known_hosts).with(
+        owner: 'root',
+        group: 'wheel',
+        mode: '0644'
+      )
+    end
+
+    it 'skips adding github rsa keys to known_hosts' do
+      expect(chef_run).not_to run_execute('add github to known_hosts').with(user: 'root')
+    end
   end
 end
