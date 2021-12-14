93dbf84f1bc8f5f3d0890f3011c61c25609fca11
James Cuzella <james.cuzella@lyraphase.com>
2021-12-13 18:04:09 -0700

    lyraphase_workstation::root_bootstrap_ssh_config: Handle root known_hosts state idempotency with `not_if`

--- recipes/root_bootstrap_ssh_config.rb
+++ recipes/root_bootstrap_ssh_config.rb
@@ -35,9 +35,9 @@ execute 'add github to known_hosts' do
   user 'root'
   cwd '/var/root/.ssh'
   command <<-SH
-    (grep github.com known_hosts) || \
     echo 'github.com,207.97.227.239 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==' >> known_hosts
   SH
+  not_if 'grep -q github.com known_hosts'
 end
 
 template '/var/root/.ssh/config' do
