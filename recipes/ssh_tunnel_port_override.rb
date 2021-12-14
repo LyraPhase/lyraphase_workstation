1f310a24b0ae1a82974374436b2f84ca2f75a5a5
James Cuzella <james.cuzella@lyraphase.com>
2021-12-13 20:25:14 -0700

    CookStyle fixups for ssh_tunnel_port_override

--- recipes/ssh_tunnel_port_override.rb
+++ recipes/ssh_tunnel_port_override.rb
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
+# frozen_string_literal: true
+
 #
-# Copyright (C) © 🄯  2018-2020 James Cuzella
-# 
+# Cookbook:: lyraphase_workstation
+# Recipe:: ssh_tunnel_port_override
+#
+# License:: GPL-3.0+
+# Copyright:: (C) © 🄯  2018-2020 James Cuzella
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -15,38 +21,37 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 ssh_tunnel_port_override = nil
-if ! node['lyraphase_workstation']['ssh_tunnel_port_override'].nil?
+unless node['lyraphase_workstation']['ssh_tunnel_port_override'].nil?
   ssh_tunnel_port_override = node['lyraphase_workstation']['ssh_tunnel_port_override']
 end
 
-file "/var/log/ssh-tunnel-port-override.log" do
+file '/var/log/ssh-tunnel-port-override.log' do
   action :create
-  owner "root"
-  group "wheel"
-  mode "0664"
+  owner 'root'
+  group 'wheel'
+  mode '0664'
 end
 
 template ssh_tunnel_port_override['script'] do
-  source "ssh-tunnel-port-override.sh.erb"
+  source 'ssh-tunnel-port-override.sh.erb'
   user node['lyraphase_workstation']['user']
-  group "wheel"
-  mode "0755"
-  variables({ ssh_tunnel_port_override_portnumber: ssh_tunnel_port_override['port'] })
+  group 'wheel'
+  mode '0755'
+  variables(ssh_tunnel_port_override_portnumber: ssh_tunnel_port_override['port'])
 end
 
-template "/Library/LaunchDaemons/com.lyraphase.ssh-tunnel-port-override.plist" do
-  source "com.lyraphase.ssh-tunnel-port-override.plist.erb"
-  user "root"
-  group "wheel"
-  mode "0644"
-  variables({ ssh_tunnel_port_override_script: ssh_tunnel_port_override['script'] })
+template '/Library/LaunchDaemons/com.lyraphase.ssh-tunnel-port-override.plist' do
+  source 'com.lyraphase.ssh-tunnel-port-override.plist.erb'
+  user 'root'
+  group 'wheel'
+  mode '0644'
+  variables(ssh_tunnel_port_override_script: ssh_tunnel_port_override['script'])
   notifies :run, 'execute[load the com.lyraphase.ssh-tunnel-port-override plist into launchd]'
 end
 
-execute "load the com.lyraphase.ssh-tunnel-port-override plist into launchd" do
-  command "launchctl load -w /Library/LaunchDaemons/com.lyraphase.ssh-tunnel-port-override.plist"
-  user "root"
+execute 'load the com.lyraphase.ssh-tunnel-port-override plist into launchd' do
+  command 'launchctl load -w /Library/LaunchDaemons/com.lyraphase.ssh-tunnel-port-override.plist'
+  user 'root'
   not_if 'sudo launchctl list com.lyraphase.ssh-tunnel-port-override'
 end
