39392f340d7906d78cf4f94178b7de24cdb15027
James Cuzella <james.cuzella@lyraphase.com>
2021-12-13 18:54:53 -0700

    CookStyle fixups for polyverse_infected_mushroom_gatekeeper

--- attributes/polyverse_infected_mushroom_gatekeeper.rb
+++ attributes/polyverse_infected_mushroom_gatekeeper.rb
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
+# frozen_string_literal: true
+
 default['lyraphase_workstation']['polyverse_infected_mushroom_gatekeeper'] = {}
 default['lyraphase_workstation']['polyverse_infected_mushroom_gatekeeper']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/InfectedMushroom-Gatekeeper-V1.3.13.dmg'
-default['lyraphase_workstation']['polyverse_infected_mushroom_gatekeeper']['dmg']['checksum']    = "dd79b805a212078a20379f1cf6409e4246ddd8fe550478dffc43003c10122208"
+default['lyraphase_workstation']['polyverse_infected_mushroom_gatekeeper']['dmg']['checksum']    = 'dd79b805a212078a20379f1cf6409e4246ddd8fe550478dffc43003c10122208'
 default['lyraphase_workstation']['polyverse_infected_mushroom_gatekeeper']['dmg']['volumes_dir'] = 'InfectedMushroom-Gatekeeper-V1.3.13'
 default['lyraphase_workstation']['polyverse_infected_mushroom_gatekeeper']['dmg']['dmg_name']    = 'InfectedMushroom-Gatekeeper-V1.3.13'
 default['lyraphase_workstation']['polyverse_infected_mushroom_gatekeeper']['dmg']['app']         = 'Gatekeeper Setup'
