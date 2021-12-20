236edcf2768a13100c8d92b03db72cce4af6cbbb
James Cuzella <james.cuzella@lyraphase.com>
2021-12-20 16:53:31 -0700

    lyraphase_workstation::root_bootstrap_ssh_config: Add UseKeychain=yes & AddKeysToAgent=yes to root ssh config

--- spec/unit/recipes/root_bootstrap_ssh_config_spec.rb
+++ spec/unit/recipes/root_bootstrap_ssh_config_spec.rb
@@ -87,6 +87,8 @@ describe 'lyraphase_workstation::root_bootstrap_ssh_config' do
       expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+PreferredAuthentications\s+publickey$/)
       expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+StrictHostKeyChecking\s+no$/)
       expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+UserKnownHostsFile\s+\/dev\/null$/)
+      expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+UseKeychain\s+yes$/)
+      expect(chef_run).to render_file(root_ssh_config).with_content(/^\s+AddKeysToAgent\s+yes$/)
     end
   end
 
