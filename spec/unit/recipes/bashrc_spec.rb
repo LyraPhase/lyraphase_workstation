b772c159727b7e34425e29aa19e493581945622a
James Cuzella <james.cuzella@lyraphase.com>
2022-01-14 00:40:23 -0700

    lyraphase_workstation::bashrc: Update Fauxhai mock to mac_os_x 10.15 for unit tests

--- spec/unit/recipes/bashrc_spec.rb
+++ spec/unit/recipes/bashrc_spec.rb
@@ -23,7 +23,7 @@ describe 'lyraphase_workstation::bashrc' do
 
   let(:chef_run) {
     klass = ChefSpec.constants.include?(:SoloRunner) ? ChefSpec::SoloRunner : ChefSpec::Runner
-    klass.new(platform: 'mac_os_x', version: '10.11.1') do |node|
+    klass.new(platform: 'mac_os_x', version: '10.15') do |node|
       create_singleton_struct "EtcPasswd", [ :name, :passwd, :uid, :gid, :gecos, :dir, :shell, :change, :uclass, :expire ]
       node.normal['etc']['passwd']['brubble'] = Struct::EtcPasswd.new('brubble', '********', 501, 20, 'Barney Rubble', '/Users/brubble', '/bin/bash', 0, '', 0)
       node.normal['lyraphase_workstation']['user'] = 'brubble'
