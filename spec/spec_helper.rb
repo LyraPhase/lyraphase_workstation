d6cac0a70a69c9b2226276285efc40693ee32490
James Cuzella <james.cuzella@lyraphase.com>
2022-02-05 02:56:08 -0700

    CookStyle fixups for spec_helper
    Offenses:
    
        spec/spec_helper.rb:40:1: C: [Corrected] Layout/TrailingBlankLines: 2 trailing blank lines detected.
        spec/spec_helper.rb:41:1: R: [Corrected] ChefDeprecations/ChefSpecCoverageReport: Don't use the deprecated ChefSpec coverage report functionality in your specs.
        at_exit { ChefSpec::Coverage.report! }
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
        1 file inspected, 2 offenses detected, 2 offenses corrected

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+# frozen_string_literal: true
+#
+# Cookbook:: lyraphase_workstation
+# Spec:: spec_helper
+#
+# License:: GPL-3.0+
+# Copyright:: (C) © 🄯  2013-2021 James Cuzella
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 require 'chefspec'
 require 'chefspec/berkshelf'
 require 'spec_shared_contexts'
