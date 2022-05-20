a99bcf230fc7716accc9eb5801c600ef153bac62
James Cuzella <james.cuzella@lyraphase.com>
2022-05-20 16:15:11 -0600

    Test Kitchen: Enable ableton_live, bashrc recipes + InSpec tests (WIP)

---
+++ test/integration/default/bashrc_test.rb
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# frozen_string_literal: true
+#
+# Cookbook Name:: lyraphase_workstation
+# Recipe:: bashrc
+# Site:: https://www.gnu.org/software/bash/
+#
+# Copyright (C) © 🄯 2016-2022 James Cuzella
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# InSpec test for recipe lyraphase_workstation::bashrc
+
+bashrc_path = '/Users/vagrant/.bashrc'
+bash_logout_path = '/Users/vagrant/.bash_logout'
+homebrew_github_api_token = 'gh_f00dcafevagrant'
+
+describe file(bashrc_path) do
+  it { should exist }
+  it { should be_file }
+  its('mode') { should cmp '0644' }
+  its('owner') { should eq 'vagrant' }
+
+  its('content') { should match Regexp.new("^export HOMEBREW_GITHUB_API_TOKEN='#{homebrew_github_api_token}'") }
+  its('content') { should match /^export DEBFULLNAME='vagrant'$/ }
+  its('content') { should match /^export DEBEMAIL='vagrant@vagrant.com'$/ }
+  its('content') { should match /^export DEBSIGN_KEYID='0xBADC0DE00FEED000'$/ }
+
+end
+
+describe file(bash_logout_path) do
+  it { should exist }
+  it { should be_file }
+  its('owner') { should eq 'vagrant' }
+  its('mode') { should cmp '0644' }
+end
+
