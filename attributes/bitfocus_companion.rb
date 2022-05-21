ef850236a9d5fc533c5abf8ed215c4b24c72eba5
James Cuzella <james.cuzella@lyraphase.com>
2022-05-20 19:38:23 -0600

    lyraphase_workstation::bitfocus_companion: Add support for Apple Silicon / M1 / arm64 architecture + ChefSpec test coverage

--- attributes/bitfocus_companion.rb
+++ attributes/bitfocus_companion.rb
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 # frozen_string_literal: true
 
+require 'chef/exceptions'
+
 # rubocop:disable Metrics/LineLength
-default['lyraphase_workstation']['bitfocus_companion']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/bitfocus-companion-2.2.0-ccea40c7-mac-x64.dmg'
-default['lyraphase_workstation']['bitfocus_companion']['dmg']['checksum']    = 'e1b7015eb4e14ad1cc7b9068fff19f53a137512fc6570e849369d15921762020'
+case node['kernel']['machine']
+when 'arm64'
+  Chef::Log.warn("lyraphase_workstation::bitfocus_companion: Detected arm64! Using Apple Silicon compatible installer")
+  default['lyraphase_workstation']['bitfocus_companion']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/bitfocus-companion-2.2.0-ccea40c7-mac-arm64.dmg'
+  default['lyraphase_workstation']['bitfocus_companion']['dmg']['checksum']    = 'ca7d293f0ada8574465f02af08110655a2b61ca3351cd6d4a0e83a34e6ea053d'
+  default['lyraphase_workstation']['bitfocus_companion']['dmg']['dmg_name']    = 'bitfocus-companion-2.2.0-ccea40c7-mac-arm64'
+when 'x86_64'
+  Chef::Log.warn("lyraphase_workstation::bitfocus_companion: Detected x86_64! Using Intel Mac compatible installer")
+  default['lyraphase_workstation']['bitfocus_companion']['dmg']['source']      = 'http://www.lyraphase.com/doc/installers/mac/bitfocus-companion-2.2.0-ccea40c7-mac-x64.dmg'
+  default['lyraphase_workstation']['bitfocus_companion']['dmg']['checksum']    = 'e1b7015eb4e14ad1cc7b9068fff19f53a137512fc6570e849369d15921762020'
+  default['lyraphase_workstation']['bitfocus_companion']['dmg']['dmg_name']    = 'bitfocus-companion-2.2.0-ccea40c7-mac-x64'
+else
+  Chef::Log.fatal("lyraphase_workstation::bitfocus_companion: Could not detect Hardware Architecture via node['kernel']['machine'] ! Could not find a compatible installer!")
+  raise Chef::Exceptions::UnsupportedPlatform.new(node['kernel']['machine'].to_s)
+end
+
 default['lyraphase_workstation']['bitfocus_companion']['dmg']['volumes_dir'] = 'Companion 2.2.0'
-default['lyraphase_workstation']['bitfocus_companion']['dmg']['dmg_name']    = 'bitfocus-companion-2.2.0-ccea40c7-mac-x64'
 default['lyraphase_workstation']['bitfocus_companion']['dmg']['app']         = 'Companion'
 default['lyraphase_workstation']['bitfocus_companion']['dmg']['type']        = 'app'
 default['lyraphase_workstation']['bitfocus_companion']['dmg']['package_id']  = 'companion.bitfocus.no'
--- spec/unit/recipes/bitfocus_companion_spec.rb
+++ spec/unit/recipes/bitfocus_companion_spec.rb
@@ -18,9 +18,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'spec_helper'
+require 'chef/exceptions'
 
 describe 'lyraphase_workstation::bitfocus_companion' do
-  let(:dmg_properties) do
+  let(:dmg_properties_x86_64) do
     {
       'source' => 'http://www.lyraphase.com/doc/installers/mac/bitfocus-companion-2.2.0-ccea40c7-mac-x64.dmg',
       'checksum' => 'e1b7015eb4e14ad1cc7b9068fff19f53a137512fc6570e849369d15921762020',
@@ -31,8 +32,19 @@ describe 'lyraphase_workstation::bitfocus_companion' do
       'type' => 'app',
     }
   end
+  let(:dmg_properties_arm64) do
+    {
+      'source' => 'http://www.lyraphase.com/doc/installers/mac/bitfocus-companion-2.2.0-ccea40c7-mac-arm64.dmg',
+      'checksum' => 'ca7d293f0ada8574465f02af08110655a2b61ca3351cd6d4a0e83a34e6ea053d',
+      'volumes_dir' => 'Companion 2.2.0',
+      'dmg_name' => 'bitfocus-companion-2.2.0-ccea40c7-mac-arm64',
+      'app' => 'Companion',
+      'pkg_id' => 'companion.bitfocus.no',
+      'type' => 'app',
+    }
+  end
 
-  context 'when given DMG attributes, on MacOS 10.15' do
+  context 'when given DMG attributes, on MacOS 10.15 with x86_64 architecture' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
     platform 'mac_os_x', '10.15'
@@ -45,7 +57,8 @@ describe 'lyraphase_workstation::bitfocus_companion' do
         node.normal['etc']['passwd']['brubble'] = Struct::EtcPasswd.new('brubble', '********', 501, 20, 'Barney Rubble', '/Users/brubble', '/bin/bash', 0, '', 0)
         node.normal['lyraphase_workstation']['user'] = 'brubble'
 
-        node.normal['lyraphase_workstation']['bitfocus_companion']['dmg'] = dmg_properties
+        node.automatic['kernel']['machine'] = 'x86_64'
+        node.normal['lyraphase_workstation']['bitfocus_companion']['dmg'] = dmg_properties_x86_64
       end.converge(described_recipe)
     end
 
@@ -53,13 +66,16 @@ describe 'lyraphase_workstation::bitfocus_companion' do
       expect { chef_run }.to_not raise_error
     end
 
-    it 'installs Dante Controller DMG' do
+    it 'installs x86_64 Bitfocus Companion DMG' do
       chef_run.converge(described_recipe)
-      expect(chef_run).to install_dmg_package(dmg_properties['volumes_dir'])
+      expect(chef_run).to install_dmg_package(dmg_properties_x86_64['volumes_dir']).with(
+          source: dmg_properties_x86_64['source'],
+          dmg_name: dmg_properties_x86_64['dmg_name']
+        )
     end
   end
 
-  context 'when run with default recipe attributes' do
+  context 'when run with default recipe attributes on x86_64 architecture' do
     let(:chef_run) do
       klass = ChefSpec.constants.include?(:SoloRunner) ? ChefSpec::SoloRunner : ChefSpec::Runner
       klass.new do |node|
@@ -67,6 +83,8 @@ describe 'lyraphase_workstation::bitfocus_companion' do
         create_singleton_struct 'EtcPasswd', [ :name, :passwd, :uid, :gid, :gecos, :dir, :shell, :change, :uclass, :expire ]
         node.normal['etc']['passwd']['brubble'] = Struct::EtcPasswd.new('brubble', '********', 501, 20, 'Barney Rubble', '/Users/brubble', '/bin/bash', 0, '', 0)
         node.normal['lyraphase_workstation']['user'] = 'brubble'
+
+        node.automatic['kernel']['machine'] = 'x86_64'
       end.converge(described_recipe)
     end
 
@@ -76,7 +94,56 @@ describe 'lyraphase_workstation::bitfocus_companion' do
 
     it 'installs DMG' do
       chef_run.converge(described_recipe)
-      expect(chef_run).to install_dmg_package(dmg_properties['volumes_dir'])
+      expect(chef_run).to install_dmg_package(dmg_properties_x86_64['volumes_dir'])
     end
   end
+
+  context 'when on MacOS 12 with Apple Silicon arm64 architecture' do
+    platform 'mac_os_x', '12'
+
+    let(:chef_run) do
+      klass = ChefSpec.constants.include?(:SoloRunner) ? ChefSpec::SoloRunner : ChefSpec::Runner
+      klass.new do |node|
+        # Struct.new("EtcPasswd", :name, :passwd, :uid, :gid, :gecos, :dir, :shell, :change, :uclass, :expire) unless ::Object.const_defined?(:EtcPasswd)
+        create_singleton_struct 'EtcPasswd', [ :name, :passwd, :uid, :gid, :gecos, :dir, :shell, :change, :uclass, :expire ]
+        node.normal['etc']['passwd']['brubble'] = Struct::EtcPasswd.new('brubble', '********', 501, 20, 'Barney Rubble', '/Users/brubble', '/bin/bash', 0, '', 0)
+        node.normal['lyraphase_workstation']['user'] = 'brubble'
+
+        node.automatic['kernel']['machine'] = 'arm64'
+      end.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+      Chef::Log.warn("Inside arm64 context: #{chef_run.node['kernel']['machine']}")
+      Chef::Log.warn("Inside arm64 context: #{chef_run.node['lyraphase_workstation']['bitfocus_companion']}")
+    end
+
+    it 'installs arm64 Bitfocus Companion DMG' do
+      chef_run.converge(described_recipe)
+      expect(chef_run).to install_dmg_package(dmg_properties_arm64['volumes_dir']).with(
+          source: dmg_properties_arm64['source'],
+          dmg_name: dmg_properties_arm64['dmg_name']
+        )
+    end
+  end
+
+  context 'when on unknown unsupported architecture: RiscV' do
+    let(:chef_run) do
+      klass = ChefSpec.constants.include?(:SoloRunner) ? ChefSpec::SoloRunner : ChefSpec::Runner
+      klass.new do |node|
+        # Struct.new("EtcPasswd", :name, :passwd, :uid, :gid, :gecos, :dir, :shell, :change, :uclass, :expire) unless ::Object.const_defined?(:EtcPasswd)
+        create_singleton_struct 'EtcPasswd', [ :name, :passwd, :uid, :gid, :gecos, :dir, :shell, :change, :uclass, :expire ]
+        node.normal['etc']['passwd']['brubble'] = Struct::EtcPasswd.new('brubble', '********', 501, 20, 'Barney Rubble', '/Users/brubble', '/bin/bash', 0, '', 0)
+        node.normal['lyraphase_workstation']['user'] = 'brubble'
+
+        node.automatic['kernel']['machine'] = 'riscv'
+      end.converge(described_recipe)
+    end
+
+    it 'raises Chef::Exceptions::UnsupportedPlatform error' do
+      expect { chef_run }.to raise_error(Chef::Exceptions::UnsupportedPlatform)
+    end
+  end
+
 end
